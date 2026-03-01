18f7f51d21c5ff01973c66f8495ecea5e0f6b19f
vidushi1129 <vidushi1129@gmail.com>
2026-03-01 21:57:54 +0530

    first commit -UC1 print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ * @author YourName
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
